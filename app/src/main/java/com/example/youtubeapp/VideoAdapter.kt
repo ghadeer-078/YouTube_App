26f91d6585d413a89a12d5b4be335102d3bd4214
ghadeer-078 <90647463+ghadeer-078@users.noreply.github.com>
2021-12-11 23:13:19 +0300

    YouTube App... Use the following 3rd party dependency to build a single activity YouTube application with a Recycler View that contains a list of videos:
    implementation 'com.pierfrancescosoffritti.androidyoutubeplayer:core:10.0.5'
    Perform an internet connection check before trying to display a video
    Use dependency documentation to understand how to control videos (Docs)
     - add a listener to gain access to controls (inherit an anonymous object to create an AbstractYouTubePlayerListener)
    Each item in the Recycler View should be clickable and play the corresponding video
    Recycler View Items should display video names.
    Edit Android Manifest to require internet connection and network state
    
    Bonus:
    Modify theme and colors
    Save video id and time stamp to allow continuous play after device rotation
    Edit Android Manifest to keep track of rotation
    Override onConfigurationChanged to track device rotation
    Set video to full screen when in landscape mode, exit full screen in portrait mode
    Add a checkbox to each card and use them to display checked videos first (This may be very challenging, can you think of a way to make it work?)
    Can you make the app more interesting?

---
+++ app/src/main/java/com/example/youtubeapp/VideoAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.youtubeapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.youtubeapp.databinding.ItemVideoBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+
+
+
+data class Video(val title:String,val code:String)
+
+
+class VideoAdapter(val videosList: List<Video>, val  player: YouTubePlayer) : RecyclerView.Adapter<VideoAdapter.ViewHolder>() {
+    class ViewHolder(val binding: ItemVideoBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemVideoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val video = videosList[position]
+
+        holder.binding.apply {
+            videoButton.text = video.title
+            videoButton.setOnClickListener {
+                player.loadVideo(video.code,0F)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = videosList.size
+
+}
